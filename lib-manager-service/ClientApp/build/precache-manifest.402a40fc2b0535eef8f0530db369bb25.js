d351c2c4c69574d7d43e4681340acb6b73164b02
Bartłomiej Chmiel <bachm44@gmail.com>
2021-03-27 23:32:59 +0100

    Add gitignore

---
+++ lib-manager-service/ClientApp/build/precache-manifest.402a40fc2b0535eef8f0530db369bb25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "071918d229f2ec9d7403b9a759c3796e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e9df6971d163a22785f",
+    "url": "/static/css/2.56e33495.chunk.css"
+  },
+  {
+    "revision": "44b0f1477a8283e70495",
+    "url": "/static/css/main.c8a63600.chunk.css"
+  },
+  {
+    "revision": "5e9df6971d163a22785f",
+    "url": "/static/js/2.3682ebc6.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.3682ebc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44b0f1477a8283e70495",
+    "url": "/static/js/main.40b92226.chunk.js"
+  },
+  {
+    "revision": "572b741d519325be2c43",
+    "url": "/static/js/runtime-main.5bec19a7.js"
+  }
+]);

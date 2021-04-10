9000669488c91cbf7ce11c66ecdd040f40749070
Bartłomiej Chmiel <bachm44@gmail.com>
2021-04-10 19:31:44 +0200

    Removed ClientApp

---
+++ lib-manager-service/Controllers/TokenController.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Web.Http;
+
+
+namespace lib_manager.Controllers
+{
+    public class TokenController : ApiController
+    {
+        [AllowAnonymous]
+        public string Get(string username, string password)
+        {
+            if (CheckUser(username, password))
+            {
+                return JwtManager.GenerateToken(username);
+            }
+
+            throw new HttpResponseException(HttpStatusCode.Unauthorized);
+        }
+
+        public bool CheckUser(string username, string password)
+        {
+            // should check in the database
+            return true;
+        }
+    }
+}
---
+++ lib-manager-service/Controllers/ValueController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+using WebApi.Jwt.Filters;
+
+namespace lib_manager.Controllers
+{
+    public class ValueController : ApiController
+    {
+        [JwtAuthentication]
+        public string Get()
+        {
+            return "value";
+        }
+    }
+}

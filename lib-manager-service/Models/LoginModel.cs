8e6b5273ef1d5d8b69820fdcdf7ae121f3a4a46b
wolfautopsy <75130868+wolfautopsy@users.noreply.github.com>
2021-04-16 20:12:44 +0200

    Adding LoginController+LoginModel

---
+++ lib-manager-service/Models/LoginModel.cs
@@ -0,0 +1,11 @@
+﻿
+namespace lib_manager.Models
+{
+
+    public class UserModel
+    {
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}

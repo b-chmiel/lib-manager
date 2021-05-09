7042c2d919405b831fa334540733c8b8176497c1
wolfautopsy <75130868+wolfautopsy@users.noreply.github.com>
2021-05-09 16:19:14 +0200

    Addition of Book Model

--- lib-manager-service/Models/BookModel.cs
+++ lib-manager-service/Models/BookModel.cs
@@ -1,7 +1,18 @@
-﻿namespace lib_manager.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace lib_manager.Models
 {
     public class BookModel
     {
-        
+        [Key]
+        public int bookId { get; set; }
+        //public int authorId { get; set; }
+        public string authorName { get; set; }
+        public string bookTitle { get; set; }
+        public string description { get; set; }
+        public string language { get; set; }
+        public DateTime publicationDate { get; set; }
+        public int pageCount { get; set; }
     }
 }

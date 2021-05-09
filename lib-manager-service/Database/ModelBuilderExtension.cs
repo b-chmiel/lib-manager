888f2fa76e7bc7e629b44cccd2c2c65fefec6456
wolfautopsy <75130868+wolfautopsy@users.noreply.github.com>
2021-05-09 16:19:38 +0200

    Updated modelbuilder to include seeding book table

--- lib-manager-service/Database/ModelBuilderExtension.cs
+++ lib-manager-service/Database/ModelBuilderExtension.cs
@@ -1,4 +1,5 @@
-﻿using lib_manager.Models;
+﻿using System;
+using lib_manager.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace lib_manager.Database
@@ -18,6 +19,35 @@ namespace lib_manager.Database
                     username = "thepope@gmail.com",
                     password = "blessyou",
                     role = UserModel.Role.Administrator
+                }); 
+            
+            modelBuilder.Entity<BookModel>().HasData(
+                new BookModel()
+                {
+                    bookId = 1,
+                    bookTitle = "Beyond Good And Evil",
+                    authorName = "Friedrich Nietzsche",
+                    description = "In Beyond Good and Evil, Nietzsche accuses past philosophers of lacking critical sense and blindly accepting " +
+                                  "dogmatic premises in their consideration of morality. Specifically, he accuses them of founding grand metaphysical " +
+                                  "systems upon the faith that the good man is the opposite of the evil man, rather than just a different expression of " +
+                                  "the same basic impulses that find more direct expression in the evil man. The work moves into the realm " +
+                                  "beyond good and evil in the sense of leaving behind the traditional morality which Nietzsche subjects to a destructive critique " +
+                                  "in favour of what he regards as an affirmative approach that fearlessly confronts the perspectival nature of knowledge and the perilous " +
+                                  "condition of the modern individual.",
+                    language = "German",
+                    publicationDate = new DateTime(1886, 6, 1),
+                    pageCount = 939
+
+                }, new BookModel()
+                {
+                    bookId = 2,
+                    bookTitle = "The Metamorphosis",
+                    authorName = "Franz Kafka",
+                    description = "The Metamorphosis tells the story of salesman Gregor Samsa, who wakes one morning to find himself inexplicably transformed " +
+                                  "into a huge insect and subsequently struggles to adjust to this new condition.",
+                    language = "German",
+                    publicationDate = new DateTime(1915, 6, 1),
+                    pageCount = 58
                 }); 
         }
     }
